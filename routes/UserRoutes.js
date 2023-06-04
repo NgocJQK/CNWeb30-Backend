996730e83200d888a29e1869e2aeca592dacceee
NghiaTM <119276144+nghiatm341@users.noreply.github.com>
2023-06-04 22:59:47 +0700

    Validate user
    - Khá ổn
    - Coi như phần auth đã xong, chỉ cần đưa vào các router khác

--- routes/UserRoutes.js
+++ routes/UserRoutes.js
@@ -1,12 +1,12 @@
 const express = require("express");
-const {
-    register,
-    login
-} = require("../controllers/UserController");
+const { register, login } = require("../controllers/UserController");
+const { registerRule, loginRule } = require("../validators/UserRule");
+const { validate } = require("../validators/Validator");
 
 const router = express.Router();
 
-router.route("/register").post(register);
-router.route("/login").post(login)
+// router.use(registerRule(), validate);
+router.route("/register").post(registerRule(), validate, register);
+router.route("/login").post(loginRule(), validate, login);
 
 module.exports = router;

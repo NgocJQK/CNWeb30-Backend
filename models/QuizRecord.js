c8c3660aa174aff6b5bed732409f2d05d200d0fd
NghiaTM <119276144+nghiatm341@users.noreply.github.com>
2023-06-16 14:56:12 +0700

    DO NOT MERGE
    - experiments

--- models/QuizRecord.js
+++ models/QuizRecord.js
@@ -1,31 +1,42 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
-const quizSchema = require("./Quiz")
+const quizSchema = require("./Quiz");
 
-const quizRecordSchema = new Schema({
+const quizRecordSchema = new Schema(
+  {
     quiz: {
-        type: Schema.Types.ObjectId,
-        ref: 'Quiz',
-        default: null
+      type: Schema.Types.ObjectId,
+      ref: "Quiz",
+      default: null,
     },
-    studentId: {
-        type: String,
-        default: "20001234"
-    },
-    studentName: {
-        type: String,
-        default: "Nguyễn Văn A"
-    },
-    isValid: {
-        type: Boolean,
-        default: true
-    },
-    note: {
-        type: String,
-        default: ""
-    }
-}, {
-  timestamps: true
-});
+    studentList: [
+      {
+        studentId: {
+          type: String,
+          default: "20001234",
+        },
+        studentName: {
+          type: String,
+          default: "Nguyễn Văn A",
+        },
+        isValid: {
+          type: Boolean,
+          default: true,
+        },
+        note: {
+          type: String,
+          default: "",
+        },
+        createAt: {
+          type: Date,
+          default: Date.now,
+        },
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
 
 module.exports = mongoose.model("QuizRecord", quizRecordSchema, "quizRecord");

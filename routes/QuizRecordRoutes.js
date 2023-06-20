92cc688bd44cc48d0bf7495d727c333028741811
NghiaTM <119276144+nghiatm341@users.noreply.github.com>
2023-06-21 00:23:39 +0700

    Resolve #35

--- routes/QuizRecordRoutes.js
+++ routes/QuizRecordRoutes.js
@@ -1,26 +1,26 @@
-const express = require('express');
+const express = require("express");
 const {
-
-    getAllQuizRecords,
-    getQuizRecordById,
-    createQuizRecord,
-    updateQuizRecord,
-    deleteQuizRecord,
+  getAllQuizRecords,
+  getQuizRecordById,
+  createQuizRecord,
+  updateQuizRecord,
+  deleteQuizRecord,
   addStudent,
   getQuizRecordByQuizId,
-} = require('../controllers/QuizRecordController');
-const { recordRule } = require('../validators/QuizRecordRule');
+} = require("../controllers/QuizRecordController");
+const { recordRule } = require("../validators/QuizRecordRule");
 
-
-const { validate } = require('../validators/Validator');
+const { validate } = require("../validators/Validator");
 const router = express.Router();
-
 
 // router.route("/").get(getAllQuizRecords).post(createQuizRecord);
 // router
 //   .route("/:id")
 //   .delete(deleteQuizRecord);
-router.route("/:quizId").get(getQuizRecordByQuizId).put(addStudent);
+router
+  .route("/:quizId")
+  .get(getQuizRecordByQuizId)
+  .put(recordRule(), validate, addStudent);
 // router.route("/:id/addStudent").put(addStudent);
 
 module.exports = router;
--- validators/QuizRecordRule.js
+++ validators/QuizRecordRule.js
@@ -1,21 +1,31 @@
-const { body } = require('express-validator');
+const { body, param } = require("express-validator");
 
-const quizRecordService = require('../services/QuizRecordService');
+const quizRecordService = require("../services/QuizRecordService");
+const quizService = require("../services/QuizService");
 
 const recordRule = () => {
-    return [
-        body('studentId')
-            .trim()
-            .exists({ value: 'falsy' })
-            .withMessage('StudentId is required')
-            .isNumeric()
-            .withMessage('Invalid studentId'),
-        body('studentName').trim().exists({ value: 'falsy' }).withMessage('StudentName is required'),
-        // body('isValid').optional().isBoolean().withMessage('Invalid isValid value'),
-        body('note').optional().isString().withMessage('Invalid note value'),
-    ];
+  return [
+    param("quizId").custom(async (quizId) => {
+      let quiz = await quizService.getQuizById(quizId);
+      if (quiz.status != "In Progress") {
+        throw new Error("Quiz expired");
+      }
+    }),
+    body("studentId")
+      .trim()
+      .exists({ value: "falsy" })
+      .withMessage("StudentId is required")
+      .isNumeric()
+      .withMessage("Invalid studentId"),
+    body("studentName")
+      .trim()
+      .exists({ value: "falsy" })
+      .withMessage("StudentName is required"),
+    // body('isValid').optional().isBoolean().withMessage('Invalid isValid value'),
+    body("note").optional().isString().withMessage("Invalid note value"),
+  ];
 };
 
 module.exports = {
-    recordRule,
+  recordRule,
 };
